417e6e4c3c856aa020ab02a2ed5591fe3e9175f7
masrobot <muhamadmashudiardiwinata@gmail.com>
2020-04-16 17:37:26 +0700

    added authentication library and create sign in activity

---
+++ app/src/main/java/c/m/aurainteriorproject/ui/signin/SignInActivity.kt
@@ -0,0 +1,13 @@
+package c.m.aurainteriorproject.ui.signin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import c.m.aurainteriorproject.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
